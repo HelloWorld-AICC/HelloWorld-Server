98540d847da6a6da9c33b3c320c8858c3c80cceb
hjh <ghkdwngns@naver.com>
2024-07-29 15:09:53 +0900

    마이페이지, 상담 전체 내역 api

---
+++ src/main/java/com/example/helloworldmvc/converter/MyPageConverter.java
@@ -0,0 +1,58 @@
+package com.example.helloworldmvc.converter;
+
+import com.example.helloworldmvc.apiPayload.GeneralException;
+import com.example.helloworldmvc.apiPayload.code.status.ErrorStatus;
+import com.example.helloworldmvc.domain.Center;
+import com.example.helloworldmvc.domain.File;
+import com.example.helloworldmvc.domain.Summary;
+import com.example.helloworldmvc.domain.User;
+import com.example.helloworldmvc.repository.UserRepository;
+import com.example.helloworldmvc.web.dto.CenterResponseDTO;
+import com.example.helloworldmvc.web.dto.MyPageResponseDTO;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class MyPageConverter {
+    public static MyPageResponseDTO.MyPageResDTO toMyPageRes(User user){
+        String userImg = null;
+
+        Optional<File> userFileOptional = Optional.ofNullable(user.getFile());
+
+        if (userFileOptional.isPresent()) {
+            userImg = userFileOptional.get().getUrl();
+        }
+        return MyPageResponseDTO.MyPageResDTO.builder()
+                .name(user.getName())
+                .userImg(userImg)
+                .build();
+
+    }
+
+    public static MyPageResponseDTO.AllSummaryRes toAllSummaryRes(Summary summary) {
+        User user = summary.getUser();
+        String userImg = null;
+        Optional<File> userFileOptional = Optional.ofNullable(user.getFile());
+        if (userFileOptional.isPresent()) {
+            userImg = userFileOptional.get().getUrl();
+        }
+        return MyPageResponseDTO.AllSummaryRes.builder()
+                .identificationNum(summary.getIdentificationNum())
+                .uploadedAt(summary.getUpdateAt())
+                .name(user.getName())
+                .userImg(userImg)
+                .title(summary.getTitle())
+                .build();
+    }
+
+    public static MyPageResponseDTO.AllSummaryListRes toAllSummaryListRes(Page<Summary> summaryList) {
+        List<MyPageResponseDTO.AllSummaryRes> allSummaryRes = summaryList.stream()
+                .map(MyPageConverter::toAllSummaryRes).collect(Collectors.toList());
+        return MyPageResponseDTO.AllSummaryListRes.builder()
+                .allsummaryList(allSummaryRes)
+                .build();
+    }
+}
